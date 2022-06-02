f09cc54efca8c99c3b86f6d9adc48b9bd1748392
KEERTHIKARAMAN <87558915+KEERTHIKARAMAN@users.noreply.github.com>
2022-06-02 17:49:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyC1xXZMZXk0pSjtZc2OLQWqS_zIZNeJe80",
+    authDomain: "classtest-1cc63.firebaseapp.com",
+    projectId: "classtest-1cc63",
+    storageBucket: "classtest-1cc63.appspot.com",
+    messagingSenderId: "118375799986",
+    appId: "1:118375799986:web:dad547baa59e0db8c23dab"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
